2562c926cdf13d8d831db515c1ed484b9e6ad83e
btmschool_repo <nayakbn00@gmail.com>
2021-04-13 22:56:27 +0530

    code update

--- jukebox_pro/urls.py
+++ jukebox_pro/urls.py
@@ -10,26 +10,34 @@ router.register(r'musician', MusiciansViewSet, basename='musician')
 router.register(r'musician_album', Music_AlbumsViewSet, basename='musician_album')
 urlpatterns = router.urls
 
-# OR
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('musicians/', views.MusicianList.as_view(), name='musicians'),
-#     path('musician_album/', views.Music_AlbumsList.as_view(), name='musician_album'),
-#     path('update/<int:id>/', views.MusiciansUpdateAPIView.as_view(), name='MusiciansUpdateAPIView'),
-#     path('update_album/<int:id>/', views.Music_AlbumsUpdateAPIView.as_view(), name='Music_AlbumsUpdateAPIView'),
-# ]
 
 ####################
+#   musician_album list => url "musician_album" request(get)
+#   musician_album Retrieve=> url "musician_album/(musician_album-id)" request(get)
+#   musician_album Create => url "/musician_album/" request(post) data_fields("musician","album_name","genre","price","description")
+#   musician_album_Update => url "musician_album/(musician_album-id)/" request(put) data_fields("musician","album_name","genre","price","description")
+#   musician_album_Delete => url "musician_album/(musician_album-id)/" request(delete)
+
 #   Musician list => url "musician" request(get)
-#   Musician Retrieve=> url "musician/id" request(get)
-#   Musician Create => url "musician" request(post) data_fields("name","musician_type")
-#   Musician_Update => url "musician/id/" request(put) data_fields("name","musician_type")
-#   Musician_Delete => url "musician/id/" request(delete)
+#   Musician Retrieve=> url "musician/(musician-id)" request(get)
+#   Musician Create => url "/musician/" request(post) data_fields("name","musician_type")
+#   Musician_Update => url "musician/(musician-id)/" request(put) data_fields("name","musician_type")
+#   Musician_Delete => url "musician/(musician-id)/" request(delete)
+
+#  List Of Music Album Sorted By date release ascending
+#              url => "musicalbum_retrieve/" request(get)
+
+# List Of Music Album for a specific musician sorted by price ascending
+#              url => "musician_filter_price/(musician-id)/" request(get)
+
+# List of Musician for specific music album sorted by Musician name ascending
+#              url => "musician_filter/(album-name)/"  request(get)
 ####################
 
 urlpatterns += [
     path('admin/', admin.site.urls),
     path('musicalbum_retrieve/', views.MusicAlbumRetrieve.as_view(), name='musicalbum_retrieve'),
-    path('musician_filter_price/<int:id>', views.MusicAlbumFilterPrice.as_view(), name='MusicAlbumFilterPrice'),
+    path('musician_filter_price/<int:id>/', views.MusicAlbumFilterPrice.as_view(), name='MusicAlbumFilterPrice'),
+    path('musician_filter/<str:name>/', views.MusicAlbumFilter.as_view(), name='MusicAlbumFilter'),
 ]
 
